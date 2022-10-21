ce0682ded480ef52510190975cd5778f8c1b743c
Pontus Alexander <pontus.alexander@gmail.com>
2022-10-21 08:51:22 +0200

    Add Sticky component to ViewStack lib

---
+++ src/components/Sticky/Sticky.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+interface StickyProps {
+  children: React.ReactNode;
+  delay?: number;
+}
+
+export default function Sticky({ children, delay = 1000 }: StickyProps) {
+  const [mount, setMount] = useState(false);
+
+  const content = useRef<React.ReactNode>(null);
+
+  if (children) {
+    content.current = children;
+  }
+
+  useEffect(() => {
+    if (children) {
+      setMount(true);
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setMount(false);
+      content.current = null;
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [children, delay]);
+
+  return <>{mount ? content.current : null}</>;
+}
